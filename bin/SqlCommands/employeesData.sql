01d31fad4b3223c4a7ee78d91e34352c1e130a96
igor <higoralves112@gmail.com>
2024-11-01 11:50:24 -0300

    sql file fix

---
+++ bin/SqlCommands/employeesData.sql
@@ -0,0 +1,13 @@
+use employees;
+create Table employees.employeesData
+(
+	employeeId int primary key,
+    lastName varchar (20),
+    salary decimal (6, 3)
+);
+
+insert into employees.employeesData (employeeId, lastName, salary) values
+    (52, "Parker", 230.000),
+    (32, "Emmich", 450.000),
+    (15, "Lopes", 200.000),
+    (21, "Vieira", 300.500)
