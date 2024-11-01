50aa68b4d799f92079d6a03d50bc0f2916f7561c
Igor Alves Reis Passos Garcia <152343992+IgorARPGarcia@users.noreply.github.com>
2024-11-01 11:54:30 -0300

    Update employeesData.sql

--- src/SqlCommands/employeesData.sql
+++ src/SqlCommands/employeesData.sql
@@ -1,13 +1,13 @@
 use employees;
 create Table employees.employeesData
 (
-		employeeId int primary key,
+	employeeId int primary key,
     	lastName varchar (20),
     	salary decimal (6, 3)
 );
 
 insert into employees.employeesData (employeeId, lastName, salary) values
-		(24, "Silva", 120.000),
+	(24, "Silva", 120.000),
     	(52, "Parker", 230.000),
     	(32, "Emmich", 450.000),
     	(15, "Lopes", 200.000),
